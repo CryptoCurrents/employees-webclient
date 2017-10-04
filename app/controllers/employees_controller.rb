604807aefc09f307c68beb39552410c1a0710986
JackMarx <seriousfools@gmail.com>
2017-10-03 21:26:45 -0500

    figaro

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,9 +1,62 @@
 class EmployeesController < ApplicationController
   def index
-    @employees = Unirest.get("localhost:3000/api/v2/employees.json").body
+    @employees = Unirest.get("#{ ENV['HOST_NAME'] }/api/v2/employees.json").body
+  end
+
+  def new
+    
+  end
+
+  def create
+    @employee = Unirest.post(
+                             "#{ ENV['HOST_NAME'] }/api/v2/employees",
+                             headers: { "Accept" => "application/json" }, 
+                             parameters:{
+                                          first_name: params[:first_name],
+                                          last_name: params[:last_name],
+                                          email: params[:email]
+                                         }
+                            ).body
+
+    redirect_to "/employees/#{@employee['id']}"
   end
 
   def show
-    @employee = Unirest.get("localhost:3000/api/v2/employees/#{params[:id]}.json").body
+    @employee = Unirest.get("#{ ENV['HOST_NAME'] }/api/v2/employees/#{params[:id]}.json").body
+  end
+
+  def edit
+    @employee = Unirest.get("#{ ENV['HOST_NAME'] }/api/v2/employees/#{params[:id]}.json").body
+  end
+
+  def update
+    @employee = Unirest.patch(
+                              "#{ ENV['HOST_NAME'] }/api/v2/employees/#{params[:id]}",
+                              headers: { "Accept" => "application/json" },
+                              parameters: {
+                                            first_name: params[:first_name],
+                                            last_name: params[:last_name],
+                                            email: params[:email]
+                                          }
+                              ).body
+    redirect_to "/employees/#{@employee['id']}"
+  end
+
+  def destroy
+    response = Unirest.delete(
+                              "#{ ENV['HOST_NAME'] }/api/v2/employees/#{params[:id]}",
+                              headers: { "Accept" => "application/json"}
+                              )
+    redirect_to '/'
   end
 end
+
+
+
+
+
+
+
+
+
+
