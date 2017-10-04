9971adcf8217578018b1fca8e558cbda4739310c
JackMarx <seriousfools@gmail.com>
2017-10-03 19:21:27 -0500

    index action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,8 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Unirest.get("localhost:3000/api/v2/employees.json").body
+  end
+
   def show
     @employee = Unirest.get("localhost:3000/api/v2/employees/#{params[:id]}.json").body
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/' => 'employees#index'
+  get '/employees' => 'employees#index'
   get '/employees/:id' => 'employees#show'
 end
